ca6ef1309c8eae9ba02bb3f5439e6300bf52b967
Angelo Ghigi <angelo@ghigi.com>
2022-12-04 08:45:08 +0100

    Day 04.2

---
+++ 04/2.php
@@ -0,0 +1,40 @@
+<?php
+
+// Init vars
+$input_file = 'input.txt';
+
+// Load input
+$handle = fopen($input_file, "r");
+if ($handle)
+{
+    $count = 0;
+
+    // Read input, line by line
+    while (($line = fgets($handle)) !== false)
+    {
+        // Get ranges
+        list($range1, $range2) = explode(',', trim($line));
+        list($min1, $max1) = explode('-', $range1);
+        $min1 = intval($min1);
+        $max1 = intval($max1);
+        list($min2, $max2) = explode('-', $range2);
+        $min2 = intval($min2);
+        $max2 = intval($max2);
+        
+        // Convert ranges to arrays
+        $range1a = range($min1, $max1);
+        $range2a = range($min2, $max2);
+
+        // Test intersection
+        $common = array_intersect($range1a, $range2a);
+
+        if (count($common))
+        {
+            $count++;
+        }
+    }
+
+    fclose($handle);
+}
+
+echo($count);
