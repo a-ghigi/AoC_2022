ff2c4f0d2c471255a160073ced13ad716d140152
Angelo Ghigi <angelo@ghigi.com>
2022-12-05 16:37:55 +0100

    Day 05.1

---
+++ 05/1.php
@@ -0,0 +1,49 @@
+<?php
+
+// Init vars
+$input_file = 'input.txt';
+
+$stack[1] = 'RGHQSBTN';
+$stack[2] = 'HSFDPZJ';
+$stack[3] = 'ZHV';
+$stack[4] = 'MZJFGH';
+$stack[5] = 'TZCDLMSR';
+$stack[6] = 'MTWVHZJ';
+$stack[7] = 'TFPLZ';
+$stack[8] = 'QVWS';
+$stack[9] = 'WHLMTDNC';
+
+// Load input
+$handle = fopen($input_file, "r");
+if ($handle)
+{
+    $matches = [];
+
+    // Read input, line by line
+    while (($line = fgets($handle)) !== false)
+    {
+        // Check input line type
+        if (preg_match('/move (\d+) from (\d+) to (\d+)/', trim($line), $matches) === 1)
+        {
+            // It's a move
+            $qty = $matches[1];
+            $from = $matches[2];
+            $to = $matches[3];
+
+            // Pop and push every element
+            for ($i = 1; $i <= $qty; $i++)
+            {
+                $stack[$to] = $stack[$to] . substr($stack[$from],strlen($stack[$from]) - 1);
+                $stack[$from] = substr($stack[$from], 0, strlen($stack[$from]) - 1);
+            }
+        }
+    }
+
+    fclose($handle);
+}
+
+// Print result
+for ($i = 1; $i <= count($stack); $i++)
+{
+    echo(substr($stack[$i], strlen($stack[$i]) - 1));
+}
